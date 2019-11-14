d71aa6a5c6484b7b497eab0687e13f6b34558b5d
Marcelo Matos dos Santos <marcelodork@hotmail.com>
2019-11-14 17:06:05 -0400

    Aula 14_11_2019

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+// Iniciando o App
 const app = express();
 
-app.get('/',(req,res) => {
- res.send('Hello world!')
-});
+//Iniciando o DB
+mongoose.connect("mongodb://mongodbapp.appdev.sanesul.ms.gov.br:27017/mongodbapp");
+
+requireDir("./src/models");
+
+const Product = mongoose.model('Product');
+
+/*app.get('/',(req,res) => {
+    Product.create({
+        title: 'React Native',
+        description: 'Build native',
+        url: 'http://teste'
+
+    });
+});*/
+
+// Primeira Rota
+app.use('/api',require('./src/routes'));
 
 app.listen(3001);
---
+++ src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const routes = express.Router();
+
+const ProductController = require('./controllers/ProductController');
+routes.get('/products',ProductController.index);
+
+/*routes.get('/products',(req,res) => {
+    Product.create({
+        title: 'React Native',
+        description: 'Build native',
+        url: 'http://teste'
+
+    });
+ return res.send('Hello world!')
+});*/
+
+/*routes.get('/products',(req,res) => {
+   
+ return res.send('Hello world!')
+});*/
+
+module.exports = routes;
